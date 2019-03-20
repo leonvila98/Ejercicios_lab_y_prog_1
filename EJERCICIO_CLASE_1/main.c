83adf66cfc641d23632d6a0b47c741aca65133ab
lionvila <38478460+lionvila@users.noreply.github.com>
2019-03-19 22:25:53 -0300

    Add files via upload

---
+++ EJERCICIO_CLASE_1/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define CANTIDAD_NUMEROS 3
+
+int pedirSumar(void);
+int promedioNumeros(void);
+
+int main()
+{
+    promedioNumeros();
+    return 0;
+}
+
+/*int pedirSumar(void)
+{
+    int valorA;
+    int valorB;
+    int resultado;
+
+    printf("\nIngrese un numero:");
+    scanf("%d", &valorA);
+    printf("\nIngrese otro numero:");
+    scanf("%d", &valorB);
+    resultado=valorA+valorB;
+    printf("%d",resultado);
+    return 0;
+}*/
+
+int promedioNumeros(void)
+{
+    int sumatoria=0;
+    int numeroIngresado;
+    int promedio;
+    int i;
+
+    for(i=0;i<=CANTIDAD_NUMEROS;i++)
+    {
+        printf("\nIngrese numero:");
+        scanf("%d",&numeroIngresado);
+        sumatoria=sumatoria+numeroIngresado;
+        promedio=sumatoria/CANTIDAD_NUMEROS;
+    }
+
+    printf("%d",promedio);
+
+    return 0;
+}
+
