37572fbe06920e7d9c653bf4be2841073a9f918c
leon <leon.18@live.com.ar>
2019-04-05 16:18:46 -0300

    funcion burbujeo y mostrar arrayInt

--- EJERCICIO_CLASE_5_B/main.c
+++ EJERCICIO_CLASE_5_B/main.c
@@ -1,36 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "utn.h"
 #define CANTIDAD_NUMEROS 5
-
-void burbujeoArrayInt(int *array,int limite);
 
 int main()
 {
-    int array[CANTIDAD_NUMEROS]={1,6,4,2,9};
+    int array[CANTIDAD_NUMEROS]={5,4,3,2,1};
     burbujeoArrayInt(array,CANTIDAD_NUMEROS);
+    mostrarArrayInt(array,CANTIDAD_NUMEROS);
 }
 
-void burbujeoArrayInt(int *array,int limite)
-{
-    int flagSwap;
-    int i;
-    int buffer;
-    do
-    {
-        for(i=0;i<limite;i++)
-        {
-            flagSwap=0;
-            if(array[i]>array[i+1])
-            {
-                buffer=array[i];
-                array[i]=array[i+1];
-                array[i+1]=buffer;
-                flagSwap=1;
-            }
-        }
-    }while(flagSwap=0);
-    for(int j=0;j<limite;j++)
-    {
-        printf("%d",array[j]);
-    }
-}
---
+++ EJERCICIO_CLASE_5_B/utn.h
@@ -0,0 +1,9 @@
+
+int getInt(char *mensaje,char *mensajeError,int minimo,int maximo,int reintentos,int *resultado);
+int isValidInt(int numero,int max,int min);
+int getFloat(char *mensaje,char *mensajeError,float minimo,float maximo,int reintentos,float *resultado);
+int isValidFloat(float numero,float max,float min);
+int getChar(char *mensaje,char *mensajeError,char minimo,char maximo,int reintentos,char *resultado);
+int isValidChar(char caracter,char min,char max);
+void mostrarArrayInt(int *array,int limite);
+void burbujeoArrayInt(int *array,int limite);
